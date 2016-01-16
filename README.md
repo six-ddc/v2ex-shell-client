72b381903067a1d52e71a03ce28b70a6cf046383
six-ddc <dongcheng.du@gmail.com>
2016-01-16 19:18:54 +0800

    增加签到、重新登录功能

--- README.md
+++ README.md
@@ -2,37 +2,40 @@
 
 ### Install:
 
-使用依赖需安装jq用于命令行解析json：
+基本使用依赖需安装jq用于命令行解析json，另外签到功能还需额外安装httpie：
 
 OS X: 
 
 ```
 brew install jq
-
+brew install httpie
 ```
 
 Ubuntu:
 
 ```
 sudo apt-get install jq
+sudo apt-get install httpie
 ```
 
-其他平台按照可参见 [jq官方文档](https://stedolan.github.io/jq/download/)
+其他平台按照可参见 [jq官方文档](https://stedolan.github.io/jq/download/)，[httpie官方文档](https://github.com/jkbrzt/httpie)
 
 ### Usage:
 
 ```
 ➜  v2ex-shell-client git:(master) ✗ ./v2ex.sh
 NONE # help
-Usage:
-        hot: 热门主题
-        late: 最新主题
-        login: 登录
-        node <nodename>: 获取节点的主题
-        cate <tech|creative|play|apple|jobs|deals|city|qna|hot|all|r2|nodes|members>: 获取指定分类的主题
-        <num>: 获取指定主题的回复列表
-        help: 查看帮助
-        q|quit: 退出
+---------------------------------------------------------------------------------------------------------------
+    hot             | 热门主题
+    late            | 最新主题
+    login/relogin   | 登录/重新登录
+    daily           | 领取每日签到奖励
+    cate <catename> | 获取指定分类的主题<tech|creative|play|apple|jobs|deals|city|qna|hot|all|r2|nodes|members>
+    node <nodename> | 获取节点的主题
+    <num>           | 查看指定主题序号的所有回复
+    help            | 查看帮助
+    q|quit          | 退出
+---------------------------------------------------------------------------------------------------------------
 ```
 
 ![image](https://raw.githubusercontent.com/six-ddc/v2ex-shell-client/master/capture/a.png)
--- v2ex.sh
+++ v2ex.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set -xe
+
 reset="\e[0m"
 
 red="\e[0;31m"
@@ -24,29 +26,39 @@ pink_back="\e[0;45m"
 cyan_back="\e[0;46m"
 
 RET=""
+HTTP=""
 
+USER_NAME=""
 MODE="none"
 ARRAY=()
 RRAY_TITLE=()
 ARRAY_CONTENT=()
 
-cookie_file="cookie.jar"
+TEMP_FILE="/tmp/ddc.v2ex.shell.tmp"
+COOKIE_FILE="cookie.jar"
+LOGIN_STATE=0
 
 _topics() {
     if [ "$1" = "topics" ]; then
-        tmpfile="/tmp/ddc.v2ex.topics.$2.json"
-        curl -s -o $tmpfile "https://www.v2ex.com/api/topics/$2.json"
+        if test "$HTTP"; then
+            $HTTP "https://www.v2ex.com/api/topics/$2.json" > $TEMP_FILE
+        else
+            curl -s -o $TEMP_FILE "https://www.v2ex.com/api/topics/$2.json"
+        fi
     elif [ "$1" = "node" ]; then
-        tmpfile="/tmp/ddc.v2ex.node.$2.json"
-        curl -s -o $tmpfile "https://www.v2ex.com/api/topics/show.json?node_name=$2"
+        if test "$HTTP"; then
+            $HTTP -f GET "https://www.v2ex.com/api/topics/show.json?node_name=$2" > $TEMP_FILE
+        else
+            curl -s -o $TEMP_FILE "https://www.v2ex.com/api/topics/show.json?node_name=$2"
+        fi
     else
         return
     fi
-    if [ `cat $tmpfile | jq -r ". | type"` != "array" ]; then
+    if [ `cat $TEMP_FILE | jq -r ". | type"` != "array" ]; then
         printf "${red}节点名不存在，另外节点名称不支持中文，如酷工作请使用jobs${reset}\n"
         return
     fi
-    LENGTH=`cat $tmpfile | jq ". | length"`
+    LENGTH=`cat $TEMP_FILE | jq ". | length"`
     if ! test $LENGTH; then
         return
     fi
@@ -56,13 +68,13 @@ _topics() {
     for((i = 0; i < $LENGTH; i++))
     do
         # 替换百分号可能引起的printf输出异常，只能在jq后解析，而不能单独通过echo解析
-        title=`jq -r ".[$i].title" $tmpfile | sed "s/\%/\%\%/g"`
-        content=`jq -r ".[$i].content" $tmpfile | sed "s/\%/\%\%/g"`
-        member=`jq -r ".[$i].member.username" $tmpfile`
-        node_title=`jq -r ".[$i].node.title" $tmpfile | sed "s/\%/\%\%/g"`
-        replies=`jq -r ".[$i].replies" $tmpfile`
+        title=`jq -r ".[$i].title" $TEMP_FILE | sed "s/\%/\%\%/g"`
+        content=`jq -r ".[$i].content" $TEMP_FILE | sed "s/\%/\%\%/g"`
+        member=`jq -r ".[$i].member.username" $TEMP_FILE`
+        node_title=`jq -r ".[$i].node.title" $TEMP_FILE | sed "s/\%/\%\%/g"`
+        replies=`jq -r ".[$i].replies" $TEMP_FILE`
         title="$blue$node_title$reset $green$title$reset $pink$member$reset($cyan$replies$reset)"
-        id=`jq -r ".[$i].id" $tmpfile`
+        id=`jq -r ".[$i].id" $TEMP_FILE`
         ARRAY[$(($i+1))]=$id
         ARRAY_TITLE[$(($i+1))]="$title"
         ARRAY_CONTENT[$(($i+1))]="$content"
@@ -96,10 +108,14 @@ _date() {
 }
 
 _categories() {
-    cate_tmpfile="/tmp/ddc.v2ex.cate.tmp"
-    curl -s -b $cookie_file -o $cate_tmpfile "https://www.v2ex.com/?tab=$1"
+    if test "$HTTP"; then
+        # $HTTP --session v2ex -f GET "https://www.v2ex.com/?tab=$1" > $TEMP_FILE
+        curl -s -b $COOKIE_FILE -o $TEMP_FILE "https://www.v2ex.com/?tab=$1"
+    else
+        curl -s -b $COOKIE_FILE -o $TEMP_FILE "https://www.v2ex.com/?tab=$1"
+    fi
     # grep的文件和重定向不能是同一个文件
-    grep '<span class="item_title">' $cate_tmpfile > $cate_tmpfile.grep
+    grep '<span class="item_title">' $TEMP_FILE > $TEMP_FILE.grep
     reg_id='<a href="/t/([0-9]+)'
     ARRAY=()
     ARRAY_TITLE=()
@@ -109,12 +125,17 @@ _categories() {
     do
         if [[ $line =~ $reg_id ]]; then
             id=${BASH_REMATCH[1]}
-            curl -s -o $cate_tmpfile "https://www.v2ex.com/api/topics/show.json?id=$id"
-            title=`jq -r ".[0].title" $cate_tmpfile | sed "s/\%/\%\%/g"`
-            content=`jq -r ".[0].content" $cate_tmpfile | sed "s/\%/\%\%/g"`
-            member=`jq -r ".[0].member.username" $cate_tmpfile`
-            node_title=`jq -r ".[0].node.title" $cate_tmpfile | sed "s/\%/\%\%/g"`
-            replies=`jq -r ".[0].replies" $cate_tmpfile`
+            if test "$HTTP"; then
+                # 对于重定向的循环中，httpie需要忽略标准输入
+                $HTTP --ignore-stdin -f GET "https://www.v2ex.com/api/topics/show.json?id=$id" > $TEMP_FILE
+            else
+                curl -s -o $TEMP_FILE "https://www.v2ex.com/api/topics/show.json?id=$id"
+            fi
+            title=`jq -r ".[0].title" $TEMP_FILE | sed "s/\%/\%\%/g"`
+            content=`jq -r ".[0].content" $TEMP_FILE | sed "s/\%/\%\%/g"`
+            member=`jq -r ".[0].member.username" $TEMP_FILE`
+            node_title=`jq -r ".[0].node.title" $TEMP_FILE | sed "s/\%/\%\%/g"`
+            replies=`jq -r ".[0].replies" $TEMP_FILE`
             title="$blue$node_title$reset $green$title$reset $pink$member$reset($cyan$replies$reset)"
             ARRAY[$i]=$id
             ARRAY_TITLE[$i]="$title"
@@ -125,7 +146,7 @@ _categories() {
                 break
             fi
         fi    
-    done < $cate_tmpfile.grep
+    done < $TEMP_FILE.grep
 }
 
 _replies() {
@@ -135,23 +156,26 @@ _replies() {
         _usage
         return
     fi
-    replies_tmpfile="/tmp/ddc.v2ex.replies.tmp"
+    replies_tmpfile="$TEMP_FILE.less"
     printf "${ARRAY_TITLE[$1]}\n${green}${ARRAY_CONTENT[$1]}${reset}\n" > $replies_tmpfile
-    tmpfile="/tmp/ddc.v2ex.replies.json"
-    curl -s -o $tmpfile "https://www.v2ex.com/api/replies/show.json?topic_id=$id"
-    LENGTH=`cat $tmpfile | jq ". | length"`
+    if test "$HTTP"; then
+        $HTTP -f GET "https://www.v2ex.com/api/replies/show.json?topic_id=$id" > $TEMP_FILE       
+    else
+        curl -s -o $TEMP_FILE "https://www.v2ex.com/api/replies/show.json?topic_id=$id"
+    fi
+    LENGTH=`cat $TEMP_FILE | jq ". | length"`
     if ! test $LENGTH; then
         return
     fi
     for((i = 0; i < $LENGTH; i++))
     do
-        content=`jq -r ".[$i].content" $tmpfile | sed "s/\%/\%\%/g"`
-        member=`jq -r ".[$i].member.username" $tmpfile`
-        created=`jq -r ".[$i].created" $tmpfile`
-        thanks=`jq -r ".[$i].thanks" $tmpfile`
+        content=`jq -r ".[$i].content" $TEMP_FILE | sed "s/\%/\%\%/g"`
+        member=`jq -r ".[$i].member.username" $TEMP_FILE`
+        created=`jq -r ".[$i].created" $TEMP_FILE`
+        thanks=`jq -r ".[$i].thanks" $TEMP_FILE`
         _date $created
         created=$RET
-        id=`jq -r ".[$i].member.id" $tmpfile`
+        id=`jq -r ".[$i].member.id" $TEMP_FILE`
         if [ $thanks != "0" ]; then
             printf "\n${blue}%3dL${reset}. $pink$member$reset $cyan$created$reset ♥️ $RED$thanks$reset\n${green}$content${reset}\n" "$(($i+1))" >> $replies_tmpfile
         else
@@ -163,22 +187,32 @@ _replies() {
 }
 
 _sel() {
-    case "$MODE" in
-        hot | late | node | cate)
-            _replies $1
-            ;;
-        *)
-            ;;
-    esac
+    if [ ${#ARRAY[@]} -gt 0 ]; then
+        _replies $1
+    else
+        printf "${red}请先获取主题列表${reset}\n"
+        _usage
+    fi
 }
 
 _login() {
-    tmpfile="/tmp/ddc.v2ex.signin.html"
     v2ex_sign='https://www.v2ex.com/signin'
-    curl -s -o $tmpfile -c $cookie_file -b $cookie_file $v2ex_sign
-    grep '登出' $tmpfile > /dev/null
+    if test "$HTTP"; then
+        if test "$1"; then
+            USER_NAME=""
+            rm -f ~/.httpie/sessions/www.v2ex.com/v2ex.json > /dev/null
+        fi
+        $HTTP --session v2ex $v2ex_sign > $TEMP_FILE
+    else
+        if test "$1"; then
+            curl -s -o $TEMP_FILE -c $COOKIE_FILE $v2ex_sign
+        else
+            curl -s -o $TEMP_FILE -c $COOKIE_FILE -b $COOKIE_FILE $v2ex_sign
+        fi
+    fi
+    grep '登出' $TEMP_FILE > /dev/null
     if [ $? != 0 ]; then
-        once=`grep 'name="once"' $tmpfile`
+        once=`grep 'name="once"' $TEMP_FILE`
         reg_once='value="([0-9]+)" name="once"'
         if [[ $once =~ $reg_once ]]; then
             once=${BASH_REMATCH[1]}
@@ -187,8 +221,12 @@ _login() {
             printf "password: "
             read -s password
             printf "\n"
-            curl -s -o $tmpfile -c $cookie_file -b $cookie_file -d "u=${username}&p=${password}&once=${once}&next=/" -e "$v2ex_sign" $v2ex_sign
-            grep '用户名和密码无法匹配' $tmpfile > /dev/null
+            if test "$HTTP"; then
+                $HTTP --session v2ex -f POST $v2ex_sign u=${username} p=${password} once=${once} next=/ Referer:$v2ex_sign > $TEMP_FILE
+            else
+                curl -s -o $TEMP_FILE -c $COOKIE_FILE -b $COOKIE_FILE -d "u=${username}&p=${password}&once=${once}&next=/" -e "$v2ex_sign" $v2ex_sign
+            fi
+            grep '用户名和密码无法匹配' $TEMP_FILE > /dev/null
             if [ $? -eq 0 ]; then
                 printf "${red}用户名或密码错误...${reset}\n"
                 return 1
@@ -199,8 +237,20 @@ _login() {
         fi
     fi
     # printf "获取用户信息...\n"
-    curl -s -o $tmpfile -b $cookie_file https://www.v2ex.com/
-    user_info=`grep "/notifications" $tmpfile`
+    if test "$HTTP"; then
+        $HTTP --session v2ex https://www.v2ex.com/ > $TEMP_FILE
+    else
+        curl -s -o $TEMP_FILE -b $COOKIE_FILE https://www.v2ex.com/
+    fi
+    user_info=`grep "bigger.*member" $TEMP_FILE`
+    reg_user='member.*>(.*)</a>'
+    if [[ $user_info =~ $reg_user ]]; then
+        USER_NAME="${BASH_REMATCH[1]}"
+    else
+        printf "${red}获取用户信息异常...${reset}\n"
+        return 1
+    fi
+    user_info=`grep "/notifications" $TEMP_FILE`
     reg_user='balance_area.*>[ ]*([0-9]+)[ ]*<img.*silver.*>[ ]*([0-9]+)[ ]*<img.*/notifications.*>(.+)</a></div>$'
     if [[ $user_info =~ $reg_user ]]; then
         silver=${BASH_REMATCH[1]}
@@ -211,10 +261,68 @@ _login() {
         fi
         printf "$green$silver 银币 $bronze 铜币 $notifi$reset\n"
     else
-        printf "${red}获取用户信息异常...${reset}\n"
+        # 只有银币的情况
+        reg_user='balance_area.*>[ ]*([0-9]+)[ ]*<img.*silver.*>.*/notifications.*>(.+)</a></div>$'
+        if [[ $user_info =~ $reg_user ]]; then
+            silver=${BASH_REMATCH[1]}
+            notifi=${BASH_REMATCH[2]}
+            if [[ notifi =~ [1-9] ]]; then
+                notifi="$notifi (https://www.v2ex.com/notifications)"
+            fi
+            printf "$green$silver 银币 $notifi$reset\n"
+        else
+            printf "${red}获取用户信息异常...${reset}\n"
+            return 1
+        fi
+    fi
+    LOGIN_STATE=1
+    return 0
+}
+
+_daily() {
+    if ! test $HTTP; then
+        printf "${red}签到需要依赖的执行程序httpie不存在，安装参考README.md${reset}\n"
         return 1
     fi
-    return 0
+    if [ $LOGIN_STATE -eq 0 ]; then
+        _login
+    fi
+    user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
+    mission_daily="https://www.v2ex.com/mission/daily"
+    if test "$HTTP"; then
+        $HTTP --session v2ex $mission_daily "Referer:https://www.v2ex.com" "User-Agent:$user_agent" > $TEMP_FILE
+    else
+        curl -s -o $TEMP_FILE -b $COOKIE_FILE -c $COOKIE_FILE -e "https://www.v2ex.com" -A "$user_agent" $mission_daily
+    fi
+    redeem=`grep 'mission/daily/redeem' $TEMP_FILE`
+    if ! test "$redeem"; then
+        printf "${green}今天已经签到...${reset}\n"
+        return 1
+    fi
+    reg_redeem="/mission/daily/redeem\?once=(.*)'"
+    if [[ $redeem =~ $reg_redeem ]]; then
+        if test "$HTTP"; then
+            $HTTP --session v2ex -f GET "https://www.v2ex.com/mission/daily/redeem?once=${BASH_REMATCH[1]}" "Referer:$mission_daily" "User-Agent:$user_agent" > $TEMP_FILE
+            $HTTP --session v2ex $mission_daily "Referer:$mission_daily" "User-Agent:$user_agent" > $TEMP_FILE
+        else
+            curl -s -o $TEMP_FILE -b $COOKIE_FILE -c $COOKIE_FILE -e $mission_daily -A "$user_agent" "https://www.v2ex.com/mission/daily/redeem?once=${BASH_REMATCH[1]}"
+            curl -s -o $TEMP_FILE -b $COOKIE_FILE -c $COOKIE_FILE -e $mission_daily -A "$user_agent" $mission_daily
+        fi
+        echo "111"
+        grep '每日登录奖励已领取' $TEMP_FILE > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "222"
+            cont=`grep '已连续登录' $TEMP_FILE`
+            reg_cont="(已连续登录.*天)"
+            if [[ $cont =~ $reg_cont ]]; then
+                echo "333"
+                printf "${green}签到成功，${BASH_REMATCH[1]}${reset}\n"
+                return 0
+            fi
+        fi
+    fi
+    printf "${red}签到异常...${reset}\n"
+    return 1
 }
 
 _test() {
@@ -222,15 +330,19 @@ _test() {
 }
 
 _usage() {
-    printf "Usage:\n"
-    printf "\thot: 热门主题\n"
-    printf "\tlate: 最新主题\n"
-    printf "\tlogin: 登录\n"
-    printf "\tcate <tech|creative|play|apple|jobs|deals|city|qna|hot|all|r2|nodes|members>: 获取指定分类的主题\n"
-    printf "\tnode <nodename>: 获取节点的主题\n"
-    printf "\t<num>: 获取指定主题的回复列表\n"
-    printf "\thelp: 查看帮助\n"
-    printf "\tq|quit: 退出\n"
+    cat << EOF
+---------------------------------------------------------------------------------------------------------------
+    hot             | 热门主题
+    late            | 最新主题
+    login/relogin   | 登录/重新登录
+    daily           | 领取每日签到奖励
+    cate <catename> | 获取指定分类的主题<tech|creative|play|apple|jobs|deals|city|qna|hot|all|r2|nodes|members>
+    node <nodename> | 获取节点的主题
+    <num>           | 查看指定主题序号的所有回复
+    help            | 查看帮助
+    q|quit          | 退出
+---------------------------------------------------------------------------------------------------------------
+EOF
 }
 
 which jq > /dev/null
@@ -239,10 +351,19 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
+which http > /dev/null
+if [ $? == 0 ]; then
+    HTTP=http
+fi
+
 while true
 do
     UPMODE=`echo $MODE | tr "[:lower:]" "[:upper:]"`
-    printf "$UPMODE # "
+    if test "$USER_NAME"; then
+        printf "($blue$USER_NAME$reset) $UPMODE # "
+    else
+        printf "$UPMODE # "
+    fi
     read data
     if ! test "$data"; then
         continue
@@ -269,11 +390,18 @@ do
                 printf "${red}使用cate <catename>格式${reset}\n"
             fi
             ;;
-        login)
-            _login
-            if [ $? -eq 0 ]; then
-                MODE=$op
+        login | relogin)
+            if [ $op = "relogin" ]; then
+                _login  1
+            else
+                _login 
             fi
+            if [ $? -eq 0 ]; then
+                MODE="LOGIN"
+            fi
+            ;;
+        daily)
+            _daily
             ;;
         node)
             node=`echo $data | cut -d " " -f 2`
@@ -293,10 +421,7 @@ do
             ;;
         *)
             if [ $op -eq $op ] 2>/dev/null ; then
-                if [ $MODE = "none" ]; then
-                    printf "${red}请先选择主题列表${reset}\n"
-                    _usage
-                fi
+                # 是数字
                 _sel $op
             else
                 _usage
